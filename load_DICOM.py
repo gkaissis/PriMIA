42bb3a9097c67f4533b0a1bc33f3ca732fa931c1
Georgios Kaissis <48105485+gkaissis@users.noreply.github.com>
2020-08-11 07:03:55 +0200

    Refactor load_DICOM to counter edge case

--- load_DICOM.py
+++ load_DICOM.py
@@ -10,11 +10,23 @@ from pathlib import Path
 from PIL import Image
 
 
-def load_dcm(f: Path) -> np.array:
-    d = pydicom.read_file(f)
+def load_dcm(filepath: Path) -> np.array:
+    """Load a DICOM located at `filepath` to a numpy array.
+
+    Args:
+        f (Path): Path of the image to load.
+
+    Returns:
+        np.array: ndarray of shape a x b x 1 channel.
+    """
+    d = pydicom.read_file(filepath)
     im_ar = d.pixel_array
     slope = float(d.RescaleSlope) if hasattr(d, "RescaleSlope") else 1
-    flip = -1 if d.PresentationLUTShape == "INVERSE" else 1
+    flip = (
+        -1
+        if (hasattr(d, "PresentationLUTShape") and d.PresentationLUTShape == "INVERSE")
+        else 1
+    )
     mult = slope * flip
     intercept = float(d.RescaleIntercept) if hasattr(d, "RescaleIntercept") else 0
     rescaled = (im_ar * mult) + intercept
