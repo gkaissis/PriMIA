4b6d31cfd855e2a825667053c0380ae6caad254c
alex <alex.ziller@tum.de>
2020-08-11 15:54:42 +0200

    inference script

--- torchlib/inference.py
+++ torchlib/inference.py
@@ -13,10 +13,12 @@ from warnings import warn
 from torchvision import datasets, transforms, models
 from argparse import Namespace
 from tqdm import tqdm
+from sklearn import metrics as mt
+from numpy import newaxis
 
 sys.path.insert(0, os.path.split(sys.path[0])[0])  # TODO: make prettier
-from utils import test, Arguments  # pylint:disable=import-error
-from torchlib.dataloader import PPPP, ImageFolderFromCSV
+from utils import stats_table, Arguments  # pylint:disable=import-error
+from torchlib.dataloader import ImageFolderFromCSV
 from torchlib.models import vgg16, resnet18, conv_at_resolution
 from torchlib.websocket_utils import read_websocket_config
 
@@ -93,6 +95,8 @@ if __name__ == "__main__":
         else:
             data_owner = sy.VirtualWorker(hook, id="data_owner")
             crypto_provider = sy.VirtualWorker(hook, id="crypto_provider")
+        workers = [sy.local_worker, data_owner]
+        # sy.local_worker.clients = [sy.local_worker, data_owner]
 
     kwargs = {"num_workers": 1, "pin_memory": True} if use_cuda else {}
     class_names = None
@@ -111,20 +115,22 @@ if __name__ == "__main__":
         )
     elif args.dataset == "pneumonia":
         num_classes = 3
+        val_mean_std = (
+            state["val_mean_std"]
+            if "val_mean_std" in state.keys()
+            else (torch.tensor([0.5]), torch.tensor([0.2]))
+            if args.pretrained
+            else (torch.tensor([0.5, 0.5, 0.5]), torch.tensor([0.2, 0.2, 0.2]))
+        )
+        mean, std = val_mean_std
         tf = [
+            # transforms.Lambda(lambda x: adaptive_hist_equalization_on_PIL(x)),
             transforms.Resize(args.inference_resolution),
             transforms.CenterCrop(args.inference_resolution),
             transforms.ToTensor(),
-            transforms.Normalize((0.57282609,), (0.17427578,)),
+            transforms.Normalize(mean.cpu(), std.cpu()),
         ]
 
-        if args.pretrained:
-            repeat = transforms.Lambda(
-                lambda x: torch.repeat_interleave(  # pylint: disable=no-member
-                    x, 3, dim=0
-                )
-            )
-            tf.append(repeat)
         if cmd_args.adults:
             testset = ImageFolderFromCSV(
                 "data/Chest_xray_Corona_Metadata.csv",
@@ -133,15 +139,24 @@ if __name__ == "__main__":
             )
 
         else:
-            testset = PPPP(
-                "data/Labels.csv", train=False, transform=transforms.Compose(tf)
+            from torchlib.dataloader import single_channel_loader
+
+            target_dict_pneumonia = {0: 1, 1: 0, 2: 2}
+            target_tf = lambda x: target_dict_pneumonia[x]
+            testset = datasets.ImageFolder(
+                "data/server_simulation/test",
+                transform=transforms.Compose(tf),
+                target_transform=target_tf,
+                loader=datasets.folder.default_loader
+                if args.pretrained
+                else single_channel_loader,
             )
         class_names = {0: "normal", 1: "bacterial pneumonia", 2: "viral pneumonia"}
     else:
         raise NotImplementedError("dataset not implemented")
 
     test_loader = torch.utils.data.DataLoader(
-        testset, batch_size=args.test_batch_size, shuffle=True, **kwargs
+        testset, batch_size=1, shuffle=True, **kwargs
     )
     if args.encrypted_inference:
         if not cmd_args.websockets_config:
@@ -175,7 +190,7 @@ if __name__ == "__main__":
             ]
             fed_dataset = sy.FederatedDataset(dist_dataset)
             test_loader = sy.FederatedDataLoader(
-                fed_dataset, batch_size=args.batch_size, shuffle=True
+                fed_dataset, batch_size=1, shuffle=False
             )
 
     if args.model == "vgg16":
@@ -207,12 +222,19 @@ if __name__ == "__main__":
     else:
         raise NotImplementedError("model unknown")
     model.load_state_dict(state["model_state_dict"])
+    model.pool, model.relu = model.relu, model.pool
+
     # model = models.vgg16(pretrained=False, num_classes=3)
     # model.classifier = vggclassifier()
     model.to(device)
-    # model = model.fix_prec().share(
-    #     sy.local_worker, data_owner, crypto_provider=crypto_provider, protocol="fss"
-    # )
+    if args.encrypted_inference:
+        fix_prec_kwargs = {"precision_fractional": 4, "dtype": "long"}
+        share_kwags = {
+            "crypto_provider": crypto_provider,
+            "protocol": "fss",
+            "requires_grad": False,
+        }
+        model.fix_precision(**fix_prec_kwargs).share(*workers, **share_kwags)
     # test method
     model.eval()
     test_loss, TP = 0, 0
@@ -224,29 +246,51 @@ if __name__ == "__main__":
             desc="performing inference",
             leave=False,
         ):
+            # data = data.get()
+            if len(data.shape) > 4:
+                data = data.squeeze(0)
+            data, target = data.to(device), target.to(device)
             if args.encrypted_inference:
-                data = data.copy()
-                data = data.fix_prec()
-                data = data.share(
-                    sy.local_worker,
-                    data_owner,
-                    crypto_provider=crypto_provider,
-                    protocol="fss",
+                data = (
+                    data.fix_precision(**fix_prec_kwargs)
+                    .share(*workers, **share_kwags)
+                    .get()
                 )
-                data = data.get()
-                data = data.squeeze()
             output = model(data)
             if args.encrypted_inference:
                 output = output.get().float_prec()
             # test_loss += loss.item()  # sum up batch loss
-            # total_scores.append(output)
-            # pred = output.argmax(dim=1)
-            # tgts = target.view_as(pred)
-            # total_pred.append(pred)
-            # total_target.append(tgts)
+            total_scores.append(output)
+            pred = output.argmax(dim=1)
+            tgts = target.view_as(pred)
+            total_pred.append(pred)
+            total_target.append(tgts)
             # equal = pred.eq(tgts)
             # TP += (
             #     equal.sum().copy().get().float_precision().long().item()
             #     if args.encrypted_inference
             #     else equal.sum().item()
             # )
+    total_pred = torch.cat(total_pred).cpu().numpy()  # pylint: disable=no-member
+    total_target = torch.cat(total_target).cpu().numpy()  # pylint: disable=no-member
+    total_scores = torch.cat(total_scores).cpu().numpy()  # pylint: disable=no-member
+    total_scores -= total_scores.min(axis=1)[:, newaxis]
+    total_scores = total_scores / total_scores.sum(axis=1)[:, newaxis]
+
+    roc_auc = mt.roc_auc_score(total_target, total_scores, multi_class="ovo")
+    objective = 100.0 * roc_auc
+    conf_matrix = mt.confusion_matrix(total_target, total_pred)
+    report = mt.classification_report(
+        total_target, total_pred, output_dict=True, zero_division=0
+    )
+    print(
+        stats_table(
+            conf_matrix,
+            report,
+            roc_auc=roc_auc,
+            matthews_coeff=mt.matthews_corrcoef(total_target, total_pred),
+            class_names=class_names,
+            epoch=0,
+        )
+    )
+
