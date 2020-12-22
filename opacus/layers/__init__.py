36a0961d263f0d828c66aba23795f1d2da0bbb97
alex <alex.ziller@tum.de>
2020-12-22 12:04:13 +0100

    removed submodule -.-

---
+++ opacus/layers/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+
+from .dp_multihead_attention import DPMultiheadAttention, SequenceBias
+from .param_rename import ParamRenamedModule
+
+
+__all__ = ["DPLSTM", "DPMultiheadAttention", "ParamRenamedModule", "SequenceBias"]
