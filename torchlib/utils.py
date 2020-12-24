fix norm calculation, indentation error

torchlib/utils.py
@@ -344,7 +344,8 @@ class MixUp(torch.nn.Module):
         self.λ = λ
 
     def forward(
-        self, x: Tuple[Union[torch.tensor, Tuple[torch.tensor]], Tuple[torch.Tensor]],
+        self,
+        x: Tuple[Union[torch.tensor, Tuple[torch.tensor]], Tuple[torch.Tensor]],
     ):
         assert len(x) == 2, "need data and target"
         x, y = x
@@ -716,7 +717,10 @@ def setup_pysyft(args, hook, verbose=False):
             # repetitions = 1 if worker.id == "validation" else args.repetitions_dataset
             if args.mixup:
                 dataset = torch.utils.data.DataLoader(
-                    dataset, batch_size=1, shuffle=True, num_workers=args.num_threads,
+                    dataset,
+                    batch_size=1,
+                    shuffle=True,
+                    num_workers=args.num_threads,
                 )
                 mixup = MixUp(λ=args.mixup_lambda, p=args.mixup_prob)
                 last_set = None
@@ -750,8 +754,12 @@ def setup_pysyft(args, hook, verbose=False):
                 selected_data = selected_data.squeeze(1)
                 selected_targets = selected_targets.squeeze(1)
             del data, targets
-            selected_data.tag("#traindata",)
-            selected_targets.tag("#traintargets",)
+            selected_data.tag(
+                "#traindata",
+            )
+            selected_targets.tag(
+                "#traintargets",
+            )
             worker.load_data([selected_data, selected_targets])
     if crypto_provider is not None:
         grid = sy.PrivateGridNetwork(*(list(workers.values()) + [crypto_provider]))
@@ -764,7 +772,8 @@ def setup_pysyft(args, hook, verbose=False):
     for worker in data.keys():
         dist_dataset = [  # TODO: in the future transform here would be nice but currently raise errors
             sy.BaseDataset(
-                data[worker][0], target[worker][0],
+                data[worker][0],
+                target[worker][0],
             )  # transform=federated_tf
         ]
         fed_dataset = sy.FederatedDataset(dist_dataset)
@@ -948,50 +957,28 @@ def dict_to_mng_dict(dictionary: dict, mng: mp.Manager):
 
 
 """
-clip_per_sample_grad_norm_, get_per_sample_norm and get_total_per_sample_grad_norm
-are all taken from here:
+clip_per_sample_grad_norm_ and get_total_per_sample_grad_norm
+adapted from:
 https://github.com/pytorch/opacus/blob/588ddf961f13981a50f8b782a11283a20d6ebc74/torchdp/per_sample_gradient_clip.py
 """
 
 
 def clip_per_sample_grad_norm_(model, max_norm):
-    r"""Clips the grad_sample stored in .grad_sample by computing a per-sample
-    norm clip factor, using it to rescale each sample's gradient in
-    .grad_sample to norm clip, then averaging them back into .grad.
-    The gradients of the model's parameters are modified in-place.
-    We assume the batch size is the first dimension.
-    Arguments:
-        tensor (Tensor): a single Tensor whose norm will be normalized
-        max_norm (float or int): max norm of the gradients
-    Returns:
-        New total norm of the tensor.
-    """
+    """Clips the gradient vector of the microbatch."""
     max_norm = float(max_norm)
     per_sample_norm = get_total_per_sample_grad_norm(model)
-
-    # Each sample gets clipped independently. This is a tensor of size B
-    per_sample_clip_factor = max_norm / (per_sample_norm + 1e-6)
-
-    # We are *clipping* the gradient, so if the factor is ever >1 we set it to 1
-    per_sample_clip_factor = per_sample_clip_factor.clamp(max=1.0)
-
-    # We recompute .grad from .grad_sample by simply averaging it over the B dim
+    per_sample_clip_factor = (max_norm / (per_sample_norm + 1e-6)).clamp(max=1.0)
     for p in model.parameters():
         g = p.grad.copy()
         p.grad.zero_()
         p.grad.add_(per_sample_clip_factor * g)
 
 
-def get_per_sample_norm(t):
-    aggregation_dims = [i for i in range(1, len(t.shape))]  # All dims except the first
-    t_squared = t * t  # elementwise
-    return torch.sqrt(t_squared.sum(dim=aggregation_dims))
-
-
 def get_total_per_sample_grad_norm(model):
-    all_layers_norms = torch.cat(
-        [get_per_sample_norm(p.grad.copy()).flatten() for p in model.parameters()]
-    )
+    """Gets the total L2 norm of the model's flattened
+    parameter vector over the microbatch.
+    """
+    all_layers_norms = torch.cat([p.grad.copy().flatten() for p in model.parameters()])
     return all_layers_norms.norm(2)
 
 
@@ -1043,7 +1030,12 @@ def train_federated(
         )
     else:
         if verbose:
-            print("Train Epoch: {} \tLoss: {:.6f}".format(epoch, avg_loss,))
+            print(
+                "Train Epoch: {} \tLoss: {:.6f}".format(
+                    epoch,
+                    avg_loss,
+                )
+            )
     return model, epsilon
 
 
@@ -1238,33 +1230,31 @@ def secure_aggregation_epoch(
                     loss = loss_fns[worker.id](output, target)
                     loss.backward()
                     with torch.no_grad():
-                        # grad_norm = get_total_per_sample_grad_norm(models[worker.id])
                         clip_per_sample_grad_norm_(
                             models[worker.id], args.max_grad_norm
                         )
                         for param in models[worker.id].parameters():
                             param.accumulated_grads.append(param.grad.clone())
-                for param in models[worker.id].parameters():
-                    if param.grad is not None:
-                        param.grad.zero_()
+                            param.grad.zero_()
                 with torch.no_grad():
                     for param in models[worker.id].parameters():
+                        noise = (
+                            torch.normal(
+                                0,
+                                args.noise_multiplier * args.max_grad_norm,
+                                size=param.grad.shape,
+                            )
+                            / args.batch_size
+                        )
+                        noise = noise.send(worker.id)
                         param.grad.add_(
                             torch.mean(  # pylint:disable=no-member
                                 torch.stack(  # pylint:disable=no-member
                                     param.accumulated_grads, dim=0
                                 )
                             )
+                            + noise
                         )
-                for p in models[worker.id].parameters():
-                    noise = (
-                        torch.normal(
-                            0, args.noise_multiplier * args.max_grad_norm, p.grad.shape,
-                        )
-                        / args.batch_size
-                    )
-                    noise = noise.send(worker.id)
-                    p.grad.add_(noise)
             else:
                 data, target = next(dataloader)
                 pred = models[worker.id](data)
@@ -1417,7 +1407,12 @@ def train(  # never called on websockets
     else:
         epsilon = 0
     if not args.visdom and verbose:
-        print("Train Epoch: {} \tLoss: {:.6f}".format(epoch, np.mean(avg_loss),))
+        print(
+            "Train Epoch: {} \tLoss: {:.6f}".format(
+                epoch,
+                np.mean(avg_loss),
+            )
+        )
     return model, epsilon
 
 
@@ -1473,7 +1468,11 @@ def stats_table(
     headers.extend(
         [class_names[i] if class_names else i for i in range(conf_matrix.shape[0])]
     )
-    return tabulate(rows, headers=headers, tablefmt="fancy_grid",)
+    return tabulate(
+        rows,
+        headers=headers,
+        tablefmt="fancy_grid",
+    )
 
 
 def test(
@@ -1528,7 +1527,11 @@ def test(
         if verbose:
             print(
                 "Test set: Epoch: {:d} Average loss: {:.4f}, Recall: {}/{} ({:.0f}%)\n".format(
-                    epoch, test_loss, TP, L, objective,
+                    epoch,
+                    test_loss,
+                    TP,
+                    L,
+                    objective,
                 ),
                 # end="",
             )